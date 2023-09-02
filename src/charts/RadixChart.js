f362d850d2daf657710cf9e79bbd1d65ec73d278
Timotej Rojko <33236065+timotejroiko@users.noreply.github.com>
2023-09-02 20:00:10 +0100

    fix angle aspects

--- src/charts/RadixChart.js
+++ src/charts/RadixChart.js
@@ -183,7 +183,7 @@ class RadixChart extends Chart {
     }
 
     fromPoints = fromPoints ?? this.#data.points
-    toPoints = toPoints ?? [...this.#data.points, {name:"AS", angle:0}, {name:"IC", angle:this.#data.cusps.at(3)}, {name:"DS", angle:180}, {name:"MC", angle:this.#data.cusps.at(9)}]
+    toPoints = toPoints ?? [...this.#data.points, {name:"AS", angle:this.#data.cusps.at(0)}, {name:"IC", angle:this.#data.cusps.at(3)}, {name:"DS", angle:this.#data.cusps.at(6)}, {name:"MC", angle:this.#data.cusps.at(9)}]
     aspects = aspects ?? this.#settings.DEFAULT_ASPECTS ?? DefaultSettings.DEFAULT_ASPECTS
 
     return AspectUtils.getAspects(fromPoints, toPoints, aspects).filter( aspect => aspect.from.name != aspect.to.name)
